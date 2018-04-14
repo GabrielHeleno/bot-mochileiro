77d0f08618d49a3c580e5ea56c6f73502a2f40c0
GabrielHeleno <38138898+GabrielHeleno@users.noreply.github.com>
2018-04-14 00:28:30 -0300

    Update bot-mochila.js

--- bot-mochila.js
+++ bot-mochila.js
@@ -8,7 +8,7 @@ bot.on('message', message => {
         message.channel.send('pong');
     }
     if(message.content.startsWith('bang')){
-        message.channel.send('{user} foi morto por um ratel de novo!');
+        message.channel.send('<@USER_ID> foi morto por um ratel de novo!');
     }
     if(message.content.startsWith('ganhei')){
       message.channel.send('parabéns, seu merda!');
