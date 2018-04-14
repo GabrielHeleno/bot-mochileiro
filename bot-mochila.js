77ce36d4a5d171cd61b999d79f00f38162e18c65
GabrielHeleno <38138898+GabrielHeleno@users.noreply.github.com>
2018-04-14 00:47:00 -0300

    Update bot-mochila.js

--- bot-mochila.js
+++ bot-mochila.js
@@ -8,13 +8,17 @@ bot.on('message', message => {
         message.channel.send('pong');
     }
     if(message.content.startsWith('bang')){
-        message.channel.send('O usuário foi morto por um ratel de novo!');
+        message.channel.send(x.channels.get('name','general')," foi morto por um ratel de novo!"+y.mention()+"!");
     }
     if(message.content.startsWith('ganhei')){
       message.channel.send('parabéns, seu merda!');
     }
 });
 
+bot.on('serverNewMember',(x,y)=>{
+    if(x === c.servers.get('id',"SERVERID"))
+        c.sendMessage(x.channels.get('name','general'),"Bem vindo ao "+y.mention()+"!")
+})
 bot.on('message', message => {
   if(message.content.startsWith('!news')){
       message.channel.send('https://newsapi.org/v2/everything?q=far%20cry&language=pt&sortBy=publishedAt&apiKey=f925e1a93a62459e8edf10242930e0a8');
